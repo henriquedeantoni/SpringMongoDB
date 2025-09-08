819a7624ce9bb6d196d70efffda99e03dabeb0db
Henrique De Antoni <henriqdeantoni@gmail.com>
2025-09-08 07:50:52 -0300

    feat: findbyid method and exception handler

--- src/main/java/com/project/springmongodb/services/UserService.java
+++ src/main/java/com/project/springmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.project.springmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.springmongodb.domain.User;
 import com.project.springmongodb.repository.UserRepository;
+import com.project.springmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
