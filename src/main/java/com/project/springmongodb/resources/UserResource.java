7e421c51d2f499d1b52c6d8f5890f38cf5d0fdb8
Henrique De Antoni <henriqdeantoni@gmail.com>
2025-09-09 13:27:26 -0300

    feat: new method for user entity

--- src/main/java/com/project/springmongodb/resources/UserResource.java
+++ src/main/java/com/project/springmongodb/resources/UserResource.java
@@ -1,14 +1,20 @@
 package com.project.springmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.project.springmongodb.domain.User;
 import com.project.springmongodb.dto.UserDTO;
@@ -32,5 +38,30 @@ public class UserResource {
 	public ResponseEntity< UserDTO> findById(@PathVariable String id){
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO userDto, @PathVariable String id){
+		User user = service.fromDTO(userDto);
+		user.setId(id);
+		user = service.update(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		
+		
+		return ResponseEntity.ok(null);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO userDto) {
+		User obj = service.fromDTO(userDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/project/springmongodb/services/UserService.java
+++ src/main/java/com/project/springmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.springmongodb.domain.User;
+import com.project.springmongodb.dto.UserDTO;
 import com.project.springmongodb.repository.UserRepository;
 import com.project.springmongodb.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,29 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+	
+	public User update(User user) {
+		User newUser = repository.findById(user.getId()).orElseThrow(() -> new RuntimeException("User not found"));
+		updateData(newUser, user);
+		return repository.save(newUser);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+	}
+	
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
 	}
 }
