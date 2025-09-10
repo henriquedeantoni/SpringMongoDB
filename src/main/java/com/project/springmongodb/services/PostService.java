046405d2ad2d6e45a86e63001fa5100a9a4bb237
Henrique De Antoni <henriqdeantoni@gmail.com>
2025-09-10 18:21:11 -0300

    feat: post endpoints update

---
+++ src/main/java/com/project/springmongodb/services/PostService.java
@@ -0,0 +1,26 @@
+package com.project.springmongodb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.springmongodb.domain.Post;
+import com.project.springmongodb.domain.User;
+import com.project.springmongodb.repository.PostRepository;
+import com.project.springmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+}
